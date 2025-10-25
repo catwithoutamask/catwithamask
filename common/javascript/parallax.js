95071dccd911efad820c181a43ae02fe6a1228c1
Cat <150280477+catwithoutamask@users.noreply.github.com>
2025-10-25 20:42:02 +0200

    enhances parallax by making the front layer stick to the right

--- common/javascript/parallax.js
+++ common/javascript/parallax.js
@@ -4,6 +4,7 @@ let stars;
 let layer1;
 let layer2;
 let layer3;
+let layer4;
 
 document.addEventListener('DOMContentLoaded', () => {
     lines = document.getElementById('lines');
@@ -12,7 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
     layer1 = document.getElementById('layer-1');
     layer2 = document.getElementById('layer-2');
     layer3 = document.getElementById('layer-3');
-    header = document.getElementById('header');
+    layer4 = document.getElementById('layer-4');
+    fitToViewportHeight(layer4)
 });
 
 window.addEventListener('scroll', function(){
@@ -24,3 +26,14 @@ window.addEventListener('scroll', function(){
     layer2.style.bottom = (value * (-0.25)) + 'px';
     layer3.style.bottom = (value * (-0.15)) + 'px';
 });
+
+function fitToViewportHeight(img) {
+  const vh = window.innerHeight;
+  const scale = vh / img.naturalHeight;
+  const renderWidth = img.naturalWidth * scale;
+  img.style.height = vh + 'px';
+  img.style.width = 'auto';
+  img.style.left = (window.innerWidth - renderWidth) + 'px';
+}
+
+window.addEventListener('resize', () => fitToViewportHeight(layer4));
